6299a6bb1407c9577dbfb5112d84c65fc0cf3697
dulovici <dulovici@Igors-MacBook-Pro.local>
2021-05-08 14:12:25 +0200

    ex 9

--- functions.js
+++ functions.js
@@ -246,14 +246,41 @@ console.log('Fumkcije');
 // "My random string" -> ["M", "y", null, "r", "a"]
 // "Random" -> ["R", "a", "n", "d", "o", "m"]
 
+// function stringToArr(str) {
+//     var newStr = [];
+//     for(var i = 0; i<str.length; i++) {
+//         if(str[i] === ' ') newStr[newStr.length] = null;
+//         else newStr[newStr.length] = str[i]
+//     }
+//     return newStr;
+// }
+// console.log(stringToArr('My random string'));
+
+
 
 // 8. Write a function that accepts a number as a parameter and checks if the number is prime or not. 
 // Note: A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+// function isPrime(num) {
+//     for(var i = 2; i < num; i++)
+//       if(num % i === 0) return false;
+//     return num > 1;
+// }
+// console.log(isPrime(8));
+//=========================================>REWIEW
+
+
 
 // 9.  Write a function that replaces spaces in a string with provided separator. If separator is not provided, use “-” (dash) as the default separator.
 // "My random string", "_" -> "My_random_string"
 // "My random string", "+" -> "My+random+string"
 // "My random string" -> "My-random-string"
+
+// function wordSeparator(str) {
+    
+// }
+// console.log(wordSeparator('Jedan dva'));
+
 
 // 10.  Write a function to get the first n characters and add “...” at the end of newly created string.
 
