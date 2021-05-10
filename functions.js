9cfb127028b1d9cc803e699a7210b96fcd4d5c08
dulovici <igorx24@gmail.com>
2021-05-10 07:02:33 +0200

    prepravi

--- functions.js
+++ functions.js
@@ -321,7 +321,7 @@ console.log('Fumkcije');
 
 // 13. Write a function to humanize a number (formats a number to a human-readable string) with the correct suffix such as 1st, 2nd, 3rd or 4th.
 // 1 -> 1st
-// 11 -> 11th
+// 11 -> 11th         =============> PREPRAVI
 
 // function humanizeNum(n) {
 //     var num = '';
