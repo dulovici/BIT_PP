e1d70e3a3499b0839866b92d99aa28688f23340a
dulovici <igorx24@gmail.com>
2021-05-13 21:05:22 +0200

    slide 5 added

--- functions.js
+++ functions.js
@@ -517,8 +517,8 @@ console.log('Fumkcije');
 // 'The quick brown fox', 'fox' -> "'fox' was found 1 times"
 // 'aa bb cc dd aa', 'aa' -> "'aa' was found 2 times"
 
-var test = 'The quick brown fox';
-var test2 = 'aa bb cc dd aa';
+// var test = 'The quick brown fox';
+// var test2 = 'aa bb cc dd aa';
 
 // function findWord(str,w) {
 //     var arr = str.split(' ');
@@ -548,7 +548,7 @@ var test2 = 'aa bb cc dd aa';
 //     return cnt;
 
 // }
-// console.log(findWrd(test2, 'aa'));
+// console.log(findWrd(test2, 'aa'));   ====> PREPRAVI RESENJE 1 NA OVU FORU BEZ SPLIT
 
 
 
@@ -561,6 +561,53 @@ var test2 = 'aa bb cc dd aa';
 
 // 10. Write a program to find the most frequent item of an array.
 // [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3].
+
+//===================================================== SLIDE 4 ===================================================================
+
+//===================================================== SLIDE 5 ===================================================================
+
+
+// 1. Find the min and max element in the following array and switch their places. Print out the
+// modified array in the console.
+// Input: [ 3, 500, 12, 149, 53, 414, 1, 19 ]
+// Output: [ 3, 1, 12, 149, 53, 414, 500, 19 ]
+
+// 2. Use the following array to make a new one by dividing its values by two and adding 5. If
+// a given element&#39;s value is 0, change it to 20.
+// Input: [ 3, 500, -10, 149, 53, 414, 1, 19 ]
+// Output: [ 6.5, 255, 20, 79.5, 31.5, 212, 5.5, 14.5 ]
+
+// 3. 
+
+// 4. Write a program that uses a loop to add all the even numbers from 1 to 1000 and
+// subtracts all the odd numbers 1 to 500 from the calculated sum. The result should then
+// be multiplied by 12.5 and displayed in console.
+// Output: 2350000
+
+// 5. Define a 10 element array. Take the first two letters from every string (that has at least 2 letters) in the array and create a new string from them. Print it out in the console.
+// Input: [ "M", "Anne", 12, "Steve", "Joe", "John", "David", "Mark", true, "A" ]
+// Output: AnStJoJoDaMa
+
+// 6. Write a program that takes a string and prints its characters out in reversed order in the console.
+// Input:  Belgrade Institute of Technology
+// Output: ygolonhceT fo etutitsnI edargleB
+
+// 7. Write a program that displays all the combinations of two numbers between 1 and 7. Don't display two of the same numbers at the same time. Display the number of possible combinations, as well. (E.g. (1.2),(2,1) is allowed, but not (1,1), (2,2)...).
+
+// 8. Write a program that checks if the entered number is a prime number (i.e. divisible only by 1 and by itself).
+// Input:  17    | 15
+// Output: true  | false
+
+// 9. Check if a given string is a palindrome (spaces are ignored).
+// Input:  eye  | Geek  | a nut for a jar of tuna
+// Output: true | false | true
+
+// 10. Write a program that calculates the greatest common divisor of two integers. Note: The greatest common divisor of two non-zero integers is the greatest positive number that divides both numbers with no remainder.
+// Input:  192 42 | 81 9
+// Output: 6      | 9
+
+
+
 
 
 
