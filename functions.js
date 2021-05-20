290e5c46d5d21418c20ee2966128d651167bb43f
BIT Student <igorx24@gmail.com>
2021-05-20 14:06:32 +0200

    implementiraj reset

--- functions.js
+++ functions.js
@@ -993,16 +993,21 @@ console.log('Fumkcije');
 
 
 
-// var btn1 = document.querySelector('.btn1');
-// var btn2 = document.querySelector('.btn2');
+var btn1 = document.querySelector('.btn1');
+var btn2 = document.querySelector('.btn2');
 
-// function createCounter(param) {
-//     var counter = 0;
+function createCounter(param) {
+    var counter = 0;
 
-//     return function () {
-//         counter += param
-//         console.log(counter);
-//     }
-// }
-// btn1.addEventListener('click', createCounter(5));
-// btn2.addEventListener('click', createCounter(1));
+    return function () {
+        counter += param
+        console.log(counter);
+    }
+}
+btn1.addEventListener('click', createCounter(5));
+btn2.addEventListener('click', createCounter(1));
+// how to reset?
+
+
+
+
