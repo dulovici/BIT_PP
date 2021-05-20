f49a03023ae35e14c550c6b75394f375d185b6b1
dulovici <igorx24@gmail.com>
2021-05-20 21:30:12 +0200

    iiidemo bre

--- functions.js
+++ functions.js
@@ -1016,31 +1016,34 @@ console.log('Fumkcije');
 
 
 
-// var btn1 = document.querySelector('.btn1');
-// var btn2 = document.querySelector('.btn2');
-// var btn3 = document.querySelector('.btn3');
+var btn1 = document.querySelector('.btn1');
+var btn2 = document.querySelector('.btn2');
+var btn3 = document.querySelector('.btn3');
 
-// function createCounter(param) {
-//     var counter = 0;
-//     var obj = {
-//         increse: function () {
-//             counter += param
-//             console.log(counter);
-//         },
-//         reset: function () {
-//             counter = 0;
-//             console.log(counter);
-//         }
-//     }
+function createCounter(param) {
+    var counter = 0;
+    btn3.addEventListener('click', function(){
+        counter = 0;
+        // console.log(couner); ???
+    });
 
-//     if (param === 0) return obj.reset
-//     return obj.increse
-// }
-// btn1.addEventListener('click', createCounter(5));
-// btn2.addEventListener('click', createCounter(1));
-// btn3.addEventListener('click', createCounter(0));
+    return function () {
+        counter += param
+        console.log(counter);
+    }
+}
 
-// how to reset?
+btn1.addEventListener('click', createCounter(5));
+btn2.addEventListener('click', createCounter(1));
+
+
+
+
+
+
+
+
+
 
 
 
