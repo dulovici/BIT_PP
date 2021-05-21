2f85baa8aa727f130e5ad6e2d2b63252ca9d023f
dulovici <igorx24@gmail.com>
2021-05-21 07:45:38 +0200

    slide6

--- functions.js
+++ functions.js
@@ -915,6 +915,80 @@ console.log('Fumkcije');
 // }
 // console.log(countVowels(test));
 
+
+
+// 2. Write a function that combines two arrays by alternatingly taking elements.
+// [‘a’,’b’,’c’], [1,2,3] -> [‘a’,1,’b’,2,’c’,3]
+
+// var arr1 =  ['a','b','c'];
+// var arr2 = [1,2,3];
+
+// function shuflle(ar1,ar2){
+//     var output = [];
+//     for(var i = 0;i<arr1.length;i++) {
+//         output[output.length] = ar1[i];
+//         output[output.length] = ar2[i];
+//     }
+//     return output;
+// }
+// console.log(shuflle(arr1,arr2));
+
+
+// 3.Write a function that rotates a list by k elements.
+// For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2]
+
+// 4.Write a function that takes a number and returns array of its digits.
+
+// function digitsOfNum(num) {
+//     num = num + '';
+//     var output = [];
+//     for(var i = 0; i<num.length; i++) {
+//         output[output.length] = Number(num[i])
+//     }
+//     return output;
+// }
+// console.log(digitsOfNum(33845));
+
+
+
+// 5.Write a program that prints a multiplication table for numbers up to 12.
+
+// 6.Write a function to input temperature in Centigrade and convert to Fahrenheit.
+
+// 7.Write a function to find the maximum element in array of numbers. Filter out all non-number elements.
+
+// 8.Write a function to find the maximum and minimum elements. Function returns an array.
+
+// 9.Write a function to find the median element of array.
+
+// 10.Write a function to find the element that occurs most frequently.
+
+// 11.Write a function to find and return the first, middle and last element of an array if the array has odd number of elements. If number of elements is even, return just the first and the last. In other cases (empty array), input array should be returned.
+
+// 12.Write a function to find the average of N elements. Make the function flexible to receive dynamic number or parameters.
+
+// 13.Write a function to find all the numbers greater than the average.
+
+// 14.The body mass index (BMI) is the ratio of the weight of a person (in kilograms) to the square of the height (in meters). Write a function that takes two parameters, weight and height, computes the BMI, and prints the corresponding BMI category:
+// Starvation: less than 15
+// Anorexic: less than 17.5
+// Underweight: less than 18.5
+// Ideal: greater than or equal to 18.5 but less than 25
+// Overweight: greater than or equal to 25 but less than 30
+// Obese: greater than or equal to 30 but less than 40
+// Morbidly obese: greater than or equal to 40
+
+// 15.Write a function that takes a list of strings and prints them, one per line, in a rectangular frame.:
+// For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:
+// *********
+// * Hello *
+// * World *
+// * in    *
+// * a     *
+// * frame *
+// *********
+
+
 //===================================================== SLIDE 7 ===================================================================
 
 
