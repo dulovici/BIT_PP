6da8de3fa4ad4425d088b4a56398dd0cbdfcfe16
dulovici <igorx24@gmail.com>
2021-05-23 08:07:56 +0200

    newslides

--- functions.js
+++ functions.js
@@ -1136,8 +1136,67 @@ console.log('Fumkcije');
 
 //===================================================== SLIDE 7 ===================================================================
 
+// 1.Why pay a fortune teller when you can just program your fortune yourself?
+// Write a function named tellFortune that:
+// takes 4 arguments: number of children, partner's name, geographic location, job title.
+// outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+// Call that function 3 times with 3 different values for the arguments.
 
+// 2.You know how old your dog is in human years, but what about dog years? Calculate it!
+// Write a function named calculateDogAge that:
+// takes 1 argument: your puppy's age.
+// calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+// outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
+// Call the function three times with different sets of values.
+// Bonus: Add an additional argument to the function that takes the conversion rate of human to dog years.
 
+// 3.Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+// Write a function named calculateSupply that:
+// takes 2 arguments: age, amount per day.
+// calculates the amount consumed for rest of the life (based on a constant max age).
+// outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
+// Call that function three times, passing in different values each time.
+// Bonus: Accept floating point values for amount per day, and round the result to a round number.
+
+// 4.It's hot out! Let's make a converter based on the steps here.
+// Create a function called celsiusToFahrenheit:
+// Store a celsius temperature into a variable.
+// Convert it to fahrenheit and output "NN°C is NN°F".
+// Create a function called fahrenheitToCelsius:
+// Now store a fahrenheit temperature into a variable.
+// Convert it to celsius and output "NN°F is NN°C."
+
+//===================================================== SLIDE 8 ===================================================================
+
+// 1.Write IIFE that replaces the first and the last element of the given array and prints out its elements. 
+// Input array: [4, 5, 11, 9]
+// Output array: [ 9, 5, 11, 4]
+
+// 2.Write IIFE that calculates the surface area of the given rectangle with sides a and b. 
+// Input: 4 5
+// Output: 20 
+
+// 3.Write IIFE that replaces all appearances of the letters m or M with * and returns the number of replacements. 
+// Input: prograMming
+// Output: progra**ing, 2
+
+// 4.Write a function with parameters name and surname that returns a function that suggests an email in the form name.surname@gmail.com. 
+// Input: pera peric
+// Output: pera.peric@gmail.com
+
+// 5.Write a function that returns a function that calculates a decimal value of the given octal number. 
+// Input: 034
+// Output: 28
+
+// 6.Write a function that checks if a given string is valid password. The password is valid if it is at least 6 characters long and contains at least one digit. The function should receive two callbacks named successCallback and errorCallback that should be called in case password is correct or invalid. 
+// Input: JSGuru 
+// Output: Your password is invalid!
+// Input: JSGuru123
+// Output: Your password is cool! 
+
+// 7.Write a function that filters elements of the given array so that they satisfy a condition given by the callback function.
+// Input: [2, 8, 11, 4, 9, 3], callback function checks if the number is odd
+// Output: [11, 9, 3] 
 
 
 
