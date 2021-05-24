23a6c2bef9343945cbf0c0e842aae4951b157f08
dulovici <igorx24@gmail.com>
2021-05-24 19:54:46 +0200

    methodes

--- functions.js
+++ functions.js
@@ -1209,6 +1209,8 @@ console.log('Fumkcije');
 // Input: pera peric
 // Output: pera.peric@gmail.com
 
+
+
 // 5.Write a function that returns a function that calculates a decimal value of the given octal number. 
 // Input: 034
 // Output: 28
@@ -1222,6 +1224,77 @@ console.log('Fumkcije');
 // 7.Write a function that filters elements of the given array so that they satisfy a condition given by the callback function.
 // Input: [2, 8, 11, 4, 9, 3], callback function checks if the number is odd
 // Output: [11, 9, 3] 
+
+//===================================================== SLIDE 9 ===================================================================
+
+//===================================================== SLIDE 10 ===================================================================
+
+// 1.Write a JavaScript function that reverses a number. typeof result of the function should be “number”.
+// 12345 -> 54321
+
+// function rever(n){
+//     var num = n + '';
+//     var output = num.split('').reverse().join('');
+
+//     return Number(output);
+// }
+// console.log(rever(12345));
+
+
+
+// 2.Write a JavaScript function that returns a passed string with letters in alphabetical order. 
+// Note: Assume punctuation, numbers and symbols are not included in the passed string.
+// “Webmaster” -> “abeemrstw”
+
+// function sortAlph(str) {
+//     return str.toLowerCase().split('').sort().join('')
+// }
+// console.log(sortAlph('Webmaster'));
+
+
+
+
+// 3.Write a function to alphabetize words of a given string. Alphabetizing a string means rearranging the letters so they are sorted from A to Z.
+// "Republic Of Serbia" -> "Rbceilpu Of Sabeir"
+
+
+
+// 4.Write a function to split a string and convert it into an array of words.
+// "John Snow" -> [ 'John', 'Snow' ]
+
+// function splitStr(str) {
+//     return str.split(' ')
+// }
+// console.log(splitStr('John Snow'));
+
+
+
+
+
+// 5.Write a function to convert a string to its abbreviated form. 
+// "John Snow" -> 	"John S."
+
+// 6.Write a function that adds string to the left or right of string, by replacing it’s characters.
+// '0000', ‘123’, 'l' -> 0123 
+// '00000000', ‘123’, 'r' -> 12300000
+
+// 7.Write a function to capitalize the first letter of a string and returns modified string.
+// "js string exercises" -> "Js string exercises"
+
+// 8.Write a function to hide email addresses to protect them from unauthorized users.
+// "somerandomaddress@example.com" -> "somerand...@example.com"
+
+// 9.Write a program that accepts a string as input and swaps the case of each character. For example, if you input 'The Quick Brown Fox', the output should be 'tHE qUICK bROWN fOX'.
+// var UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// var LOWER = 'abcdefghijklmnopqrstuvwxyz';
+// "The Quick Brown Fox" -> "tHE qUICK bROWN fOX"
+
+
+
+
+
+
+
 
 //===================================================== CODE WARS ===================================================================
 
