7c407ebffb66389ef6cc1e51b7c008daea844bc0
dulovici <igorx24@gmail.com>
2021-08-08 14:41:37 +0200

    11. Square evety digit

--- codeWars.js
+++ codeWars.js
@@ -233,3 +233,23 @@ console.log('Code Wars');
 
 
 
+
+//11. Square evety digit 
+//Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+//For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// function squareDigits(num){
+//     const output = [];
+//     const numSplit = num.
+//     toString()
+//     .split('')
+//     .map(i => +i)
+//     .forEach(el => {
+//         output.push(el * el)
+//     })
+    
+//     return +output.join('')
+// }
+
+// console.log(squareDigits(9119));
+
